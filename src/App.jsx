18edd37bddd332099de0f2f3be62405e03e16581
lwky <abdirsk04@hotmail.com>
2025-10-21 19:53:50 +0200

    Endret på package.json, lagt til react på alle jsx filer og sjekker om det kommer på console.log.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./app.css";
+
+export default function App() {
+  console.log("Tester om det funker");
+  return (
+    <main>
+      <h1>Work finder</h1>
+    </main>
+  );
+}
